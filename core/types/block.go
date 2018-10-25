b880292d2c28a6fad80eaa61c999af00ffe45c11
Sonic <sonic@cobinhood.com>
2018-10-25 17:54:24 +0800

    core: types: fix header marshal json

--- core/types/block.go
+++ core/types/block.go
@@ -101,6 +101,8 @@ type headerMarshaling struct {
 	GasUsed    hexutil.Uint64
 	Time       *hexutil.Big
 	Extra      hexutil.Bytes
+	Randomness hexutil.Bytes
+	DexconMeta hexutil.Bytes
 	Hash       common.Hash `json:"hash"` // adds call to Hash() in MarshalJSON
 }
 
