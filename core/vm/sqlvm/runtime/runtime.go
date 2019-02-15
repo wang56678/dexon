587cf85238455ae5387ba4e7b44b6e5f8386d936
JM <jm@dexon.org>
2019-02-15 16:43:33 +0800

    core: vm: sqlvm: export instruction's member (#197)

--- core/vm/sqlvm/runtime/runtime.go
+++ core/vm/sqlvm/runtime/runtime.go
@@ -8,12 +8,12 @@ import (
 // Run is runtime entrypoint.
 func Run(stateDB vm.StateDB, ins []Instruction, registers []*Operand) (ret []byte, err error) {
 	for _, in := range ins {
-		opFunc := jumpTable[in.op]
-		err = opFunc(&common.Context{}, in.input, registers, in.output)
+		opFunc := jumpTable[in.Op]
+		err = opFunc(&common.Context{}, in.Input, registers, in.Output)
 		if err != nil {
 			return nil, err
 		}
 	}
-	// TODO: ret = ABIEncode(ins[len(ins)-1].output)
+	// TODO: ret = ABIEncode(ins[len(ins)-1].Output)
 	return
 }
