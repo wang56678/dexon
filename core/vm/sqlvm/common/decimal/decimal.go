2c6825730b6eee30cc0f79414940e74625fe0d38
Meng-Ying Yang <garfield@dexon.org>
2019-02-23 10:51:51 +0800

    core: vm: sqlvm: opcodes and basic structs
    For runtime implementation, we define opcodes and basic structs for
    runtime usage and concrete implementation.

--- core/vm/sqlvm/common/decimal/decimal.go
+++ core/vm/sqlvm/common/decimal/decimal.go
@@ -6,4 +6,9 @@ import "github.com/dexon-foundation/decimal"
 var (
 	False = decimal.New(0, 0)
 	True  = decimal.New(1, 0)
+
+	Int64Max = decimal.New(1, 63).Sub(decimal.One)
+	Int64Min = decimal.New(1, 63).Neg()
+
+	UInt16Max = decimal.New(1, 16).Sub(decimal.One)
 )
--- core/vm/sqlvm/runtime/runtime.go
+++ core/vm/sqlvm/runtime/runtime.go
@@ -3,20 +3,25 @@ package runtime
 import (
 	"github.com/dexon-foundation/dexon/core/vm"
 	"github.com/dexon-foundation/dexon/core/vm/sqlvm/common"
-	"github.com/dexon-foundation/dexon/core/vm/sqlvm/errors"
+	se "github.com/dexon-foundation/dexon/core/vm/sqlvm/errors"
 )
 
 // Run is runtime entrypoint.
 func Run(stateDB vm.StateDB, ins []Instruction, registers []*Operand) (ret []byte, err error) {
 	for _, in := range ins {
+		for i := 0; i < len(in.Input); i++ {
+			if !in.Input[i].IsImmediate {
+				in.Input[i] = registers[in.Input[i].RegisterIndex]
+			}
+		}
 		opFunc := jumpTable[in.Op]
 		loadRegister(in.Input, registers)
 		errCode := opFunc(&common.Context{}, in.Input, registers, in.Output)
 		if errCode != nil {
-			err = errors.Error{
+			err = se.Error{
 				Position: in.Position,
-				Code:     errCode.(errors.ErrorCode),
-				Category: errors.ErrorCategoryRuntime,
+				Code:     errCode.(se.ErrorCode),
+				Category: se.ErrorCategoryRuntime,
 			}
 			return nil, err
 		}
