064101432404ac837ce16f2a4a229862a97e0bdd
Meng-Ying Yang <garfield@dexon.org>
2019-03-05 16:34:05 +0800

    core: vm: sqlvm: add jump table

--- core/vm/sqlvm/runtime/jumptable.go
+++ core/vm/sqlvm/runtime/jumptable.go
@@ -1,6 +1,32 @@
 package runtime
 
 var jumpTable = [256]OpFunction{
+	// 0x10
+	ADD: opAdd,
+	MUL: opMul,
+	SUB: opSub,
+	DIV: opDiv,
+	MOD: opMod,
+
+	// 0x20
+	LT:    opLt,
+	GT:    opGt,
+	EQ:    opEq,
+	AND:   opAnd,
+	OR:    opOr,
+	NOT:   opNot,
+	UNION: opUnion,
+	INTXN: opIntxn,
+	LIKE:  opLike,
+
+	// 0x40
+	ZIP:    opZip,
+	FIELD:  opField,
+	PRUNE:  opPrune,
+	SORT:   opSort,
+	FILTER: opFilter,
+	CAST:   opCast,
+
 	// 0x60
 	LOAD: opLoad,
 }
